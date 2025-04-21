897f1d036024bed81aead57faf895939043e9c2e
Dan Carber <whasian63@gmail.com>
2025-04-21 17:24:58 -0600

    bug fix

--- cafana/NuMINue2025_MC.cc
+++ cafana/NuMINue2025_MC.cc
@@ -60,7 +60,7 @@ int main()
      * by the Tree class to store the results of the analysis.
      */
     #define CUT cuts::nue::all_1eNp_cut
-    #define TCUT cuts::no_cut//cuts::neutrino
+    #define TCUT cuts::neutrino
     std::map<std::string, ana::SpillMultiVar> vars_selected_nu_phase;
     vars_selected_nu_phase.insert({"nu_id", SpineVar<TTYPE,RTYPE>(&vars::neutrino_id, &CUT, &TCUT)});
     //vars_selected_nu_phase.insert({"CutType", SpineVar<RTYPE,RTYPE>(&vars::nue::cut_type, &CUT, &TCUT)}); // GUNDAM

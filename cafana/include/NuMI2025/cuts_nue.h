e21fc6b91e790851a6f11126dba83af96a0b283d
Dan Carber <whasian63@gmail.com>
2025-04-22 15:11:09 -0600

    Adding quality cuts

--- cafana/include/NuMI2025/cuts_nue.h
+++ cafana/include/NuMI2025/cuts_nue.h
@@ -31,7 +31,24 @@
  */
 namespace cuts::nue
 {
+    /**
+     * @brief Apply a fiducial volume, containment, flash time (BNB), and 1muNp
+     * topological cut (logical "and" of each).
+     * @details This function applies a fiducial volume, containment, flash time
+     * (BNB), and 1muNp topological cut on the interaction using the logical "and"
+     * of each previously defined cut.
+     * @tparam T the type of interaction (true or reco).
+     * @param obj the interaction to select on.
+     * @return true if the interaction passes the fiducial volume, containment,
+     * flash time, and 1muNp topological cut.
+     * @note This cut is intended to be used for the muon2024 analysis.
+     */
+    template<class T>
+        bool quality_cuts(const T & obj) { 
+            size_t i(utilities::leading_particle_index(obj, 1));
 
+            return obj.particles[i].axial_spread >0.02;
+        }
     /**
      * @brief Apply a 1mu1p topological (final state) cut.
      * @details The interaction must have a topology matching 1mu1p as defined by
@@ -201,23 +218,6 @@ namespace cuts::nue
      */
     bool nonsignal_1eX(const caf::SRInteractionTruthDLPProxy & obj) { return cuts::neutrino(obj) && !(cuts::fiducial_cut(obj) && cuts::track_containment_cut(obj)) && topological_1eX_cut(obj); }
 
-    /**
-     * @brief Apply a fiducial volume, containment, flash time (BNB), and 1muNp
-     * topological cut (logical "and" of each).
-     * @details This function applies a fiducial volume, containment, flash time
-     * (BNB), and 1muNp topological cut on the interaction using the logical "and"
-     * of each previously defined cut.
-     * @tparam T the type of interaction (true or reco).
-     * @param obj the interaction to select on.
-     * @return true if the interaction passes the fiducial volume, containment,
-     * flash time, and 1muNp topological cut.
-     * @note This cut is intended to be used for the muon2024 analysis.
-     */
-    template<class T>
-        bool quality_cuts(const T & obj) { 
-            size_t i(utilities::leading_particle_index(obj, 1));
-
-            return obj.particles[i].axial_spread >0.02;
-        }
+    
 }
 #endif // CUTS_MUON2024_H

638b76da6e8a6fda15c643f09fd708be7353383b
Dan Carber <whasian63@gmail.com>
2025-04-23 15:57:23 -0600

    Adding efficiency selection

--- cafana/include/NuMI2025/cuts_nue.h
+++ cafana/include/NuMI2025/cuts_nue.h
@@ -178,7 +178,6 @@ namespace cuts::nue
      * @note This cut is intended to be used for the muon2024 analysis for
      * defining the signal.
      */
-    template<class T>
     bool signal_1eNp(const caf::SRInteractionTruthDLPProxy & obj) { return cuts::neutrino(obj) && cuts::fiducial_cut(obj) && cuts::track_containment_cut(obj) && topological_1eNp_cut(obj); }
 
     /**

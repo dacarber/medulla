5c12c535da7e5c5d9601a84c7f4ac7b630d65d95
Dan Carber <whasian63@gmail.com>
2025-04-24 11:36:05 -0600

    Adding efficiency selection

--- cafana/include/NuMI2025/cuts_nue.h
+++ cafana/include/NuMI2025/cuts_nue.h
@@ -15,8 +15,8 @@
 #include <cmath>
 #include <algorithm>
 
-//#include "include/utilities.h"
-#include "include/NuMI2025/utilities_nue.h"
+#include "include/utilities.h"
+//#include "include/NuMI2025/utilities_nue.h"
 //#include "include/cuts.h"
 
 

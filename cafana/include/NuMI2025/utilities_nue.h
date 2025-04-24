b77ae24c2d0e589feed55c680ffad2189dc5603c
Dan Carber <whasian63@gmail.com>
2025-04-24 11:52:33 -0600

    Adding efficiency selection

--- cafana/include/NuMI2025/utilities_nue.h
+++ cafana/include/NuMI2025/utilities_nue.h
@@ -85,7 +85,7 @@ struct reco_inter {
  * vars and cuts namespaces, which are used for organizing variables and cuts
  * which act on interactions.
  */
-namespace utilities_nue
+namespace utilities::nue
 {
     /**
      * @brief Check if the particle meets final state signal requirements.

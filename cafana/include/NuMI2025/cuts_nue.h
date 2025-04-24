39513669885d308ed46b3d53446e959c9cc4405f
Dan Carber <whasian63@gmail.com>
2025-04-24 11:46:40 -0600

    Adding efficiency selection

--- cafana/include/NuMI2025/cuts_nue.h
+++ cafana/include/NuMI2025/cuts_nue.h
@@ -31,7 +31,7 @@
  * @note The namespace is intended to be used in conjunction with the cuts
  * namespace, which is used for organizing generic cuts which act on interactions.
  */
-namespace cuts::nue
+namespace cut::nue
 {
     /**
      * @brief Apply a fiducial volume, containment, flash time (BNB), and 1muNp
@@ -118,7 +118,7 @@ namespace cuts::nue
      * @note This cut is intended to be used for the muon2024 analysis.
      */
     template<class T>
-        bool all_1e1p_cut(const T & obj) { return cuts::fiducial_cut<T>(obj) && track_containment_cut<T>(obj) && cuts::flash_cut<T>(obj) && topological_1e1p_cut<T>(obj); }
+        bool all_1e1p_cut(const T & obj) { return fiducial_cut<T>(obj) && track_containment_cut<T>(obj) && flash_cut<T>(obj) && topological_1e1p_cut<T>(obj); }
 
     /**
      * @brief Apply a fiducial volume, containment, flash time (BNB), and 1muNp
@@ -133,7 +133,7 @@ namespace cuts::nue
      * @note This cut is intended to be used for the muon2024 analysis.
      */
     template<class T>
-        bool all_1eNp_cut(const T & obj) { return cuts::fiducial_cut<T>(obj) && cuts::track_containment_cut<T>(obj) && cuts::flash_cut<T>(obj) && topological_1eNp_cut<T>(obj) ; }
+        bool all_1eNp_cut(const T & obj) { return fiducial_cut<T>(obj) && cuts::track_containment_cut<T>(obj) && flash_cut<T>(obj) && topological_1eNp_cut<T>(obj) ; }
 
     /**
      * @brief Apply a fiducial volume, containment, flash time (BNB), and 1muX
@@ -148,7 +148,7 @@ namespace cuts::nue
      * @note This cut is intended to be used for the muon2024 analysis.
      */
     template<class T>
-        bool all_1eX_cut(const T & obj) { return cuts::fiducial_cut<T>(obj) && cuts::track_containment_cut<T>(obj) && cuts::flash_cut<T>(obj) && topological_1eX_cut<T>(obj); }//quality_cuts<T>(obj)
+        bool all_1eX_cut(const T & obj) { return fiducial_cut<T>(obj) && track_containment_cut<T>(obj) && flash_cut<T>(obj) && topological_1eX_cut<T>(obj); }//quality_cuts<T>(obj)
 
     /**
      * @brief Apply a cut to select the 1mu1p signal.
@@ -160,7 +160,7 @@ namespace cuts::nue
      * @note This cut is intended to be used for the muon2024 analysis for
      * defining the signal.
      */
-    bool signal_1e1p(const caf::SRInteractionTruthDLPProxy & obj) { return cuts::neutrino(obj) && cuts::fiducial_cut(obj) && cuts::track_containment_cut(obj) && topological_1e1p_cut(obj); }
+    bool signal_1e1p(const caf::SRInteractionTruthDLPProxy & obj) { return neutrino(obj) && fiducial_cut(obj) && track_containment_cut(obj) && topological_1e1p_cut(obj); }
 
     /**
      * @brief Apply a cut to select the 1mu1p non-signal.
@@ -173,7 +173,7 @@ namespace cuts::nue
      * @note This cut is intended to be used for the muon2024 analysis for
      * defining a complement to the signal.
      */
-    bool nonsignal_1e1p(const caf::SRInteractionTruthDLPProxy & obj) { return cuts::neutrino(obj) && !(cuts::fiducial_cut(obj) && cuts::containment_cut(obj)) && topological_1e1p_cut(obj); }
+    bool nonsignal_1e1p(const caf::SRInteractionTruthDLPProxy & obj) { return neutrino(obj) && !(fiducial_cut(obj) && containment_cut(obj)) && topological_1e1p_cut(obj); }
 
     /**
      * @brief Apply a cut to select the 1muNp signal.
@@ -200,7 +200,7 @@ namespace cuts::nue
      * @note This cut is intended to be used for the muon2024 analysis for
      * defining a complement to the signal.
      */
-    bool nonsignal_1eNp(const caf::SRInteractionTruthDLPProxy & obj) { return cuts::neutrino(obj) && !(cuts::fiducial_cut(obj) && cuts::track_containment_cut(obj)) && topological_1eNp_cut(obj); }
+    bool nonsignal_1eNp(const caf::SRInteractionTruthDLPProxy & obj) { return neutrino(obj) && !(fiducial_cut(obj) && track_containment_cut(obj)) && topological_1eNp_cut(obj); }
 
     /**
      * @brief Apply a cut to select the 1muX signal.
@@ -213,7 +213,7 @@ namespace cuts::nue
      * @note This cut is intended to be used for the muon2024 analysis for
      * defining the signal.
      */
-    bool signal_1eX(const caf::SRInteractionTruthDLPProxy & obj) { return cuts::neutrino(obj) && cuts::fiducial_cut(obj) && cuts::track_containment_cut(obj) && topological_1eX_cut(obj); }
+    bool signal_1eX(const caf::SRInteractionTruthDLPProxy & obj) { return neutrino(obj) && fiducial_cut(obj) && track_containment_cut(obj) && topological_1eX_cut(obj); }
 
     /**
      * @brief Apply a cut to select the 1muX non-signal.
@@ -226,7 +226,7 @@ namespace cuts::nue
      * @note This cut is intended to be used for the muon2024 analysis for
      * defining a complement to the signal.
      */
-    bool nonsignal_1eX(const caf::SRInteractionTruthDLPProxy & obj) { return cuts::neutrino(obj) && !(cuts::fiducial_cut(obj) && cuts::track_containment_cut(obj)) && topological_1eX_cut(obj); }
+    bool nonsignal_1eX(const caf::SRInteractionTruthDLPProxy & obj) { return neutrino(obj) && !(fiducial_cut(obj) && track_containment_cut(obj)) && topological_1eX_cut(obj); }
 
     
 }

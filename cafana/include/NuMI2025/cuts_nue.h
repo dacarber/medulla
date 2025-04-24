db2609e5101dfce3970ed79ee3ca3bcc2b73eeb1
Dan Carber <whasian63@gmail.com>
2025-04-24 12:23:15 -0600

    Adding efficiency selection

--- cafana/include/NuMI2025/cuts_nue.h
+++ cafana/include/NuMI2025/cuts_nue.h
@@ -181,9 +181,9 @@ namespace cuts::nue
      * defining the signal.
      */
     bool signal_1eNp(const caf::SRInteractionTruthDLPProxy & obj) { 
-     //   truth_inter s = utilities_nue::truth_interaction_info(obj);
-      //  return s.num_primary_electrons_thresh == 1 && s.num_primary_protons_thresh > 0 && s.num_primary_photons_thresh == 0 && s.num_primary_muons_thresh == 0 && s.num_primary_pions_thresh == 0 && s.is_neutrino && s.has_contained_tracks && s.is_fiducial; }
-        return false;}
+        truth_inter s = utilities_nue::truth_interaction_info(obj);
+        return s.num_primary_electrons_thresh == 1 && s.num_primary_protons_thresh > 0 && s.num_primary_photons_thresh == 0 && s.num_primary_muons_thresh == 0 && s.num_primary_pions_thresh == 0 && s.is_neutrino && s.has_contained_tracks && s.is_fiducial; }
+        //return false;}
     /**
      * @brief Apply a cut to select the 1muNp non-signal.
      * @details This function applies a cut on the final state, fiducial volume,

272ba3eaae6eefe1cd38e9b74627d9a531342cb4
Dan Carber <whasian63@gmail.com>
2025-04-30 13:34:24 -0600

    Adding systematics

--- systematics/include/trees.h
+++ systematics/include/trees.h
@@ -380,6 +380,8 @@ namespace sys::trees
             {
                 for(const caf::SRTrueInteraction & nu : mc)
                 {
+                    std::cout<<std::trunc(nu.E*100000)<<" : "<<candidates.find(index)<<endl;
+
                     index_t index(*rrun, *rsubrun, *revt, nu.index, std::trunc(nu.E*100000));
                     if(candidates.find(index) != candidates.end())
                     {

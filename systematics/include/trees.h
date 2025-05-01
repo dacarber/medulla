64944b11be8c060e35ac9bb4fc9ec081c641cd15
Dan Carber <whasian63@gmail.com>
2025-05-01 13:38:05 -0600

    Adding systematics

--- systematics/include/trees.h
+++ systematics/include/trees.h
@@ -219,12 +219,15 @@ namespace sys::trees
          * process streamlines the copying of the values from the input TTree to
          * the output TTree.
          */
+        bool matches_energy, matches_baseline;
         TTree * output_tree = new TTree(table.get_string_field("name").c_str(), table.get_string_field("name").c_str());
         for(auto & br : brs)
             output_tree->Branch(br.first.c_str(), &br.second);
         output_tree->Branch("Run", &run);
         output_tree->Branch("Subrun", &subrun);
         output_tree->Branch("Evt", &event);
+        output_tree->Branch("matches_energy", &matches_energy);
+        output_tree->Branch("matches_baseline", &matches_baseline);
         
         /**
          * @brief Create the map of selected signal candidates.

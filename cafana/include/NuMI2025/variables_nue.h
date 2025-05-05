158f33c7747ba1efe48de1890496df4212939d81
Dan Carber <whasian63@gmail.com>
2025-05-05 10:18:29 -0600

    Add cut w/o quality cuts

--- cafana/include/NuMI2025/variables_nue.h
+++ cafana/include/NuMI2025/variables_nue.h
@@ -118,18 +118,16 @@ namespace vars::nue
     double truth_topology(const caf::SRInteractionTruthDLPProxy & interaction)
     {   
         double topo(0);
-        if(interaction.nu_id >= 0){
             std::vector<uint32_t> counts(utilities::count_primaries(interaction));
             topo = count[0]*1e8 + count[1]*1e6 + count[2]*1e4 + count[3]*1e2 + count[4];
-        return topo;
     }
 
     double reco_topology(const T & interaction)
     {   
         double topo(0);
-        if(interaction.nu_id >= 0){
             std::vector<uint32_t> counts(utilities::count_primaries(interaction));
             topo = count[0]*1e8 + count[1]*1e6 + count[2]*1e4 + count[3]*1e2 + count[4];
+
         return topo;
     }
     /**
